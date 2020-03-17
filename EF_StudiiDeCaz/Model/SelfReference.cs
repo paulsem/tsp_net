deac99b188813b0ae42aa4b1f378822c79c66064
paulsem <32594551+paulsem@users.noreply.github.com>
2020-03-17 14:38:28 +0200

    Add files via upload

---
+++ EF_StudiiDeCaz/Model/SelfReference.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace EF_StudiiDeCaz.Model
+{
+    public class SelfReference
+    {
+
+        public int SelfeReferenceId { get; private set; }
+        public string Name { get; set; }
+
+        public int? ParentSelfReferencedId { get; private set; }
+
+        public SelfReference ParentSelfReference { get; set; }
+
+        public virtual ICollection<SelfReference> References { get; set; }
+        public SelfReference()
+        {
+            References = new HashSet<SelfReference>();
+        }
+        
+    }
+}
