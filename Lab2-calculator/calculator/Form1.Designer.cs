91e06bd0af47b7163c68c8a784d6b2ebb77f43ba
paulsem <32594551+paulsem@users.noreply.github.com>
2020-03-23 12:16:25 +0200

    Add files via upload

---
+++ Lab2-calculator/calculator/Form1.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.n1 = new System.Windows.Forms.Button();
+            this.n2 = new System.Windows.Forms.Button();
+            this.n3 = new System.Windows.Forms.Button();
+            this.n4 = new System.Windows.Forms.Button();
+            this.n5 = new System.Windows.Forms.Button();
+            this.n6 = new System.Windows.Forms.Button();
+            this.n7 = new System.Windows.Forms.Button();
+            this.n8 = new System.Windows.Forms.Button();
+            this.n9 = new System.Windows.Forms.Button();
+            this.n0 = new System.Windows.Forms.Button();
+            this.adunare = new System.Windows.Forms.Button();
+            this.scadere = new System.Windows.Forms.Button();
+            this.impartire = new System.Windows.Forms.Button();
+            this.radical = new System.Windows.Forms.Button();
+            this.rezultat = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.clear = new System.Windows.Forms.Button();
+            this.inmultire = new System.Windows.Forms.Button();
+            this.factorial = new System.Windows.Forms.Button();
+            this.power_2 = new System.Windows.Forms.Button();
+            this.ln = new System.Windows.Forms.Button();
+            this.power_y = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // n1
+            // 
+            this.n1.Location = new System.Drawing.Point(197, 213);
+            this.n1.Name = "n1";
+            this.n1.Size = new System.Drawing.Size(70, 70);
+            this.n1.TabIndex = 0;
+            this.n1.Text = "1";
+            this.n1.UseVisualStyleBackColor = true;
+            this.n1.Click += new System.EventHandler(this.n1_Click);
+            // 
+            // n2
+            // 
+            this.n2.Location = new System.Drawing.Point(273, 213);
+            this.n2.Name = "n2";
+            this.n2.Size = new System.Drawing.Size(70, 70);
+            this.n2.TabIndex = 1;
+            this.n2.Text = "2";
+            this.n2.UseVisualStyleBackColor = true;
+            this.n2.Click += new System.EventHandler(this.n2_Click_1);
+            // 
+            // n3
+            // 
+            this.n3.Location = new System.Drawing.Point(349, 213);
+            this.n3.Name = "n3";
+            this.n3.Size = new System.Drawing.Size(70, 70);
+            this.n3.TabIndex = 2;
+            this.n3.Text = "3";
+            this.n3.UseVisualStyleBackColor = true;
+            this.n3.Click += new System.EventHandler(this.n3_Click_1);
+            // 
+            // n4
+            // 
+            this.n4.Location = new System.Drawing.Point(197, 137);
+            this.n4.Name = "n4";
+            this.n4.Size = new System.Drawing.Size(70, 70);
+            this.n4.TabIndex = 3;
+            this.n4.Text = "4";
+            this.n4.UseVisualStyleBackColor = true;
+            this.n4.Click += new System.EventHandler(this.n4_Click);
+            // 
+            // n5
+            // 
+            this.n5.Location = new System.Drawing.Point(273, 137);
+            this.n5.Name = "n5";
+            this.n5.Size = new System.Drawing.Size(70, 70);
+            this.n5.TabIndex = 4;
+            this.n5.Text = "5";
+            this.n5.UseVisualStyleBackColor = true;
+            this.n5.Click += new System.EventHandler(this.n5_Click);
+            // 
+            // n6
+            // 
+            this.n6.Location = new System.Drawing.Point(349, 137);
+            this.n6.Name = "n6";
+            this.n6.Size = new System.Drawing.Size(70, 70);
+            this.n6.TabIndex = 5;
+            this.n6.Text = "6";
+            this.n6.UseVisualStyleBackColor = true;
+            this.n6.Click += new System.EventHandler(this.n6_Click_1);
+            // 
+            // n7
+            // 
+            this.n7.Location = new System.Drawing.Point(197, 61);
+            this.n7.Name = "n7";
+            this.n7.Size = new System.Drawing.Size(70, 70);
+            this.n7.TabIndex = 6;
+            this.n7.Text = "7";
+            this.n7.UseVisualStyleBackColor = true;
+            this.n7.Click += new System.EventHandler(this.n7_Click_1);
+            // 
+            // n8
+            // 
+            this.n8.Location = new System.Drawing.Point(273, 61);
+            this.n8.Name = "n8";
+            this.n8.Size = new System.Drawing.Size(70, 70);
+            this.n8.TabIndex = 7;
+            this.n8.Text = "8";
+            this.n8.UseVisualStyleBackColor = true;
+            this.n8.Click += new System.EventHandler(this.n8_Click);
+            // 
+            // n9
+            // 
+            this.n9.Location = new System.Drawing.Point(349, 61);
+            this.n9.Name = "n9";
+            this.n9.Size = new System.Drawing.Size(70, 70);
+            this.n9.TabIndex = 8;
+            this.n9.Text = "9";
+            this.n9.UseVisualStyleBackColor = true;
+            this.n9.Click += new System.EventHandler(this.n9_Click);
+            // 
+            // n0
+            // 
+            this.n0.Location = new System.Drawing.Point(90, 213);
+            this.n0.Name = "n0";
+            this.n0.Size = new System.Drawing.Size(70, 70);
+            this.n0.TabIndex = 9;
+            this.n0.Text = "0";
+            this.n0.UseVisualStyleBackColor = true;
+            this.n0.Click += new System.EventHandler(this.n0_Click);
+            // 
+            // adunare
+            // 
+            this.adunare.Location = new System.Drawing.Point(14, 61);
+            this.adunare.Name = "adunare";
+            this.adunare.Size = new System.Drawing.Size(70, 70);
+            this.adunare.TabIndex = 10;
+            this.adunare.Text = "+";
+            this.adunare.UseVisualStyleBackColor = true;
+            this.adunare.Click += new System.EventHandler(this.adunare_Click);
+            // 
+            // scadere
+            // 
+            this.scadere.Location = new System.Drawing.Point(90, 61);
+            this.scadere.Name = "scadere";
+            this.scadere.Size = new System.Drawing.Size(70, 70);
+            this.scadere.TabIndex = 11;
+            this.scadere.Text = "-";
+            this.scadere.UseVisualStyleBackColor = true;
+            this.scadere.Click += new System.EventHandler(this.scadere_Click);
+            // 
+            // impartire
+            // 
+            this.impartire.Location = new System.Drawing.Point(14, 137);
+            this.impartire.Name = "impartire";
+            this.impartire.Size = new System.Drawing.Size(70, 70);
+            this.impartire.TabIndex = 12;
+            this.impartire.Text = "/";
+            this.impartire.UseVisualStyleBackColor = true;
+            this.impartire.Click += new System.EventHandler(this.impartire_Click);
+            // 
+            // radical
+            // 
+            this.radical.Location = new System.Drawing.Point(14, 213);
+            this.radical.Name = "radical";
+            this.radical.Size = new System.Drawing.Size(70, 70);
+            this.radical.TabIndex = 13;
+            this.radical.Text = "sqrt";
+            this.radical.UseVisualStyleBackColor = true;
+            this.radical.Click += new System.EventHandler(this.radical_Click);
+            // 
+            // rezultat
+            // 
+            this.rezultat.Location = new System.Drawing.Point(425, 137);
+            this.rezultat.Name = "rezultat";
+            this.rezultat.Size = new System.Drawing.Size(70, 146);
+            this.rezultat.TabIndex = 14;
+            this.rezultat.Text = "=";
+            this.rezultat.UseVisualStyleBackColor = true;
+            this.rezultat.Click += new System.EventHandler(this.rezultat_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(14, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.textBox1.Size = new System.Drawing.Size(481, 22);
+            this.textBox1.TabIndex = 15;
+            this.textBox1.Text = "0";
+            // 
+            // clear
+            // 
+            this.clear.Location = new System.Drawing.Point(425, 61);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(70, 70);
+            this.clear.TabIndex = 16;
+            this.clear.Text = "C";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // inmultire
+            // 
+            this.inmultire.Location = new System.Drawing.Point(90, 137);
+            this.inmultire.Name = "inmultire";
+            this.inmultire.Size = new System.Drawing.Size(70, 70);
+            this.inmultire.TabIndex = 17;
+            this.inmultire.Text = "*";
+            this.inmultire.UseVisualStyleBackColor = true;
+            this.inmultire.Click += new System.EventHandler(this.inmultire_Click);
+            // 
+            // factorial
+            // 
+            this.factorial.Location = new System.Drawing.Point(14, 289);
+            this.factorial.Name = "factorial";
+            this.factorial.Size = new System.Drawing.Size(70, 70);
+            this.factorial.TabIndex = 18;
+            this.factorial.Text = "n!";
+            this.factorial.UseVisualStyleBackColor = true;
+            this.factorial.Click += new System.EventHandler(this.factorial_Click);
+            // 
+            // power_2
+            // 
+            this.power_2.Location = new System.Drawing.Point(90, 289);
+            this.power_2.Name = "power_2";
+            this.power_2.Size = new System.Drawing.Size(70, 70);
+            this.power_2.TabIndex = 19;
+            this.power_2.Text = "x^2";
+            this.power_2.UseVisualStyleBackColor = true;
+            this.power_2.Click += new System.EventHandler(this.power_2_Click);
+            // 
+            // ln
+            // 
+            this.ln.Location = new System.Drawing.Point(197, 289);
+            this.ln.Name = "ln";
+            this.ln.Size = new System.Drawing.Size(70, 70);
+            this.ln.TabIndex = 20;
+            this.ln.Text = "log10";
+            this.ln.UseVisualStyleBackColor = true;
+            this.ln.Click += new System.EventHandler(this.ln_Click);
+            // 
+            // power_y
+            // 
+            this.power_y.Location = new System.Drawing.Point(273, 289);
+            this.power_y.Name = "power_y";
+            this.power_y.Size = new System.Drawing.Size(70, 70);
+            this.power_y.TabIndex = 21;
+            this.power_y.Text = "x^y";
+            this.power_y.UseVisualStyleBackColor = true;
+            this.power_y.Click += new System.EventHandler(this.power_y_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(654, 384);
+            this.Controls.Add(this.power_y);
+            this.Controls.Add(this.ln);
+            this.Controls.Add(this.power_2);
+            this.Controls.Add(this.factorial);
+            this.Controls.Add(this.inmultire);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.rezultat);
+            this.Controls.Add(this.radical);
+            this.Controls.Add(this.impartire);
+            this.Controls.Add(this.scadere);
+            this.Controls.Add(this.adunare);
+            this.Controls.Add(this.n0);
+            this.Controls.Add(this.n9);
+            this.Controls.Add(this.n8);
+            this.Controls.Add(this.n7);
+            this.Controls.Add(this.n6);
+            this.Controls.Add(this.n5);
+            this.Controls.Add(this.n4);
+            this.Controls.Add(this.n3);
+            this.Controls.Add(this.n2);
+            this.Controls.Add(this.n1);
+            this.Name = "Form1";
+            this.Text = "Calculator Stiintific";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button n1;
+        private System.Windows.Forms.Button n2;
+        private System.Windows.Forms.Button n3;
+        private System.Windows.Forms.Button n4;
+        private System.Windows.Forms.Button n5;
+        private System.Windows.Forms.Button n6;
+        private System.Windows.Forms.Button n7;
+        private System.Windows.Forms.Button n8;
+        private System.Windows.Forms.Button n9;
+        private System.Windows.Forms.Button n0;
+        private System.Windows.Forms.Button adunare;
+        private System.Windows.Forms.Button scadere;
+        private System.Windows.Forms.Button impartire;
+        private System.Windows.Forms.Button radical;
+        private System.Windows.Forms.Button rezultat;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.Button inmultire;
+        private System.Windows.Forms.Button factorial;
+        private System.Windows.Forms.Button power_2;
+        private System.Windows.Forms.Button ln;
+        private System.Windows.Forms.Button power_y;
+    }
+}
+
---
+++ Lab2-calculator/calculator/Form1.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculator
+{
+    public partial class Form1 : Form
+    {
+        double First_Number;
+        string Operatie;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool nonNumberEntered = false;
+
+        // Handle the KeyDown event to determine the type of character entered into the control.
+        private void textBox1_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+            // Initialize the flag to false.
+            nonNumberEntered = false;
+
+            // Determine whether the keystroke is a number from the top of the keyboard.
+            if (e.KeyCode < Keys.D0 || e.KeyCode > Keys.D9)
+            {
+                // Determine whether the keystroke is a number from the keypad.
+                if (e.KeyCode < Keys.NumPad0 || e.KeyCode > Keys.NumPad9)
+                {
+                    // Determine whether the keystroke is a backspace.
+                    if (e.KeyCode != Keys.Back)
+                    {
+                        // A non-numerical keystroke was pressed.
+                        // Set the flag to true and evaluate in KeyPress event.
+                        nonNumberEntered = true;
+                    }
+                }
+            }
+            //If shift key was pressed, it's not a number.
+            if (Control.ModifierKeys == Keys.Shift)
+            {
+                nonNumberEntered = true;
+            }
+        }
+        private void textBox1_KeyPress(object sender, System.Windows.Forms.KeyPressEventArgs e)
+        {
+            // Check for the flag being set in the KeyDown event.
+            if (nonNumberEntered == true)
+            {
+                // Stop the character from being entered into the control since it is non-numerical.
+                e.Handled = true;
+            }
+        }
+
+
+
+        private void n1_Click(object sender, EventArgs e)
+        {
+           
+            if(textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "1";
+            }
+            else
+            {
+                textBox1.Text += "1";
+            }
+        }
+        private void n2_Click_1(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "2";
+            }
+            else
+            {
+                textBox1.Text += "2";
+            }
+        }
+        private void n3_Click_1(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "3";
+            }
+            else
+            {
+                textBox1.Text += "3";
+            }
+        }
+
+        private void n4_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "4";
+            }
+            else
+            {
+                textBox1.Text += "4";
+            }
+        }
+        private void n5_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "5";
+            }
+            else
+            {
+                textBox1.Text += "5";
+            }
+        }
+        private void n6_Click_1(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "6";
+            }
+            else
+            {
+                textBox1.Text += "6";
+            }
+        }
+
+        private void n7_Click_1(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "7";
+            }
+            else
+            {
+                textBox1.Text += "7";
+            }
+        }
+
+        private void n8_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "8";
+            }
+            else
+            {
+                textBox1.Text += "8";
+            }
+        }
+
+        private void n9_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "9";
+            }
+            else
+            {
+                textBox1.Text += "9";
+            }
+        }
+        private void n0_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "0";
+            }
+            else
+            {
+                textBox1.Text += "0";
+            }
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+        }
+
+        private void adunare_Click(object sender, EventArgs e)
+        {
+            First_Number = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = "0";
+            Operatie = "+";
+        }
+
+        private void scadere_Click(object sender, EventArgs e)
+        {
+            First_Number = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = "0";
+            Operatie = "-";
+        }
+
+        private void impartire_Click(object sender, EventArgs e)
+        {
+            First_Number = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = "0";
+            Operatie = "/";
+        }
+
+        private void radical_Click(object sender, EventArgs e)
+        {
+            double x;
+            First_Number = Convert.ToDouble(textBox1.Text);
+            x = Math.Sqrt(First_Number);
+            textBox1.Text = Convert.ToString(x);
+            First_Number = x;
+          
+        }
+
+
+        private void inmultire_Click(object sender, EventArgs e)
+        {
+            First_Number = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = "0";
+            Operatie = "*";
+        }
+
+        private void rezultat_Click(object sender, EventArgs e)
+        {
+            double Second_Number = Convert.ToDouble(textBox1.Text);
+            double Result;
+            if(Operatie == "power")
+            {
+                Result = Math.Pow(First_Number, Second_Number);
+                textBox1.Text = Convert.ToString(Result);
+                First_Number = Result;
+            }
+            if(Operatie == "+")
+            {
+                Result = First_Number + Second_Number;
+                textBox1.Text = Convert.ToString(Result);
+                First_Number = Result;
+            }
+            if(Operatie == "-")
+            {
+                Result = First_Number - Second_Number;
+                textBox1.Text = Convert.ToString(Result);
+                First_Number = Result;
+            }
+            if(Operatie == "/")
+            {
+                Result = First_Number / Second_Number;
+                textBox1.Text = Convert.ToString(Result);
+                First_Number = Result;
+            }
+      
+            if(Operatie == "*")
+            {
+                Result = First_Number * Second_Number;
+                textBox1.Text = Convert.ToString(Result);
+                First_Number = Result;
+            }
+        }
+
+        private void factorial_Click(object sender, EventArgs e)
+        {
+            double x = Convert.ToInt32(textBox1.Text);
+            double Result = 1;
+        
+            for (int i = 2; i <= x; i++)
+            {
+                Result *= i;
+            }
+
+            textBox1.Text = Convert.ToString(Result);
+
+        }
+
+        private void power_2_Click(object sender, EventArgs e)
+        {
+            double x = Convert.ToDouble(textBox1.Text);
+            double Result = Math.Pow(x, 2);
+            textBox1.Text = Convert.ToString(Result);
+        }
+
+        private void ln_Click(object sender, EventArgs e)
+        {
+            double x = Convert.ToDouble(textBox1.Text);
+            double Result = Math.Log10(x);
+            textBox1.Text = Convert.ToString(Result);
+        }
+
+        private void power_y_Click(object sender, EventArgs e)
+        {
+            First_Number = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = "0";
+            Operatie = "power";
+        }
+    }
+}
