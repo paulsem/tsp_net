2dd55306c0eaf92f073182320a3aafe6a8345f26
paulsem <32594551+paulsem@users.noreply.github.com>
2020-03-23 16:19:53 +0200

    Add files via upload

---
+++ Lab4-ModelCodeFirst/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ModelCodeFirst.Model.Model1;
+
+namespace ModelCodeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new AlbumsContext())
+            {
+                var albums = new Album { AlbumName = "Album nou" };
+                db.Albums.Add(albums);
+                db.SaveChanges();
+
+                var query = from b in db.Albums
+                            orderby b.AlbumName
+                            select b;
+                Console.WriteLine("All albums in database");
+                foreach(var item in query)
+                {
+                    Console.WriteLine(item.AlbumName);
+                }
+                Console.WriteLine("Press any key to exit");
+                Console.ReadKey();
+            }
+        }
+    }
+
+   
+}
