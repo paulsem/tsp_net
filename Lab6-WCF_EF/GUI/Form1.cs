a1bcd22b554ad2003876dfd4419e9b2791941543
paulsem <32594551+paulsem@users.noreply.github.com>
2020-03-24 12:35:44 +0200

    Laborator 6
    Laborator 6

---
+++ Lab6-WCF_EF/GUI/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Lab6_WCF_EF;
+
+namespace GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            posts = LoadPosts().ToList<Post>();
+            dgp.DataSource = posts;
+            dgp.Columns[0].Width = 0;
+            if (dgp.Rows.Count > 0)
+                dgc.DataSource = posts[0].Comments;
+        }
+        private static PostComment.Post[] LoadPosts()
+        {
+            PostCommentClient pc = new PostCommentClient();
+            PostComment.Post[] p = pc.GetPosts();
+            return p;
+        }
+    }
+}
---
+++ Lab6-WCF_EF/ObjectWCF/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab6_WCF_EF;
+
+
+namespace ObjectWCF
+{
+    public class Class1
+    {
+    }
+}
