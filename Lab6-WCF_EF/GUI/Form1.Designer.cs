ea8a6a31d199888f6247f355de3daa27e4ad6db6
paulsem <32594551+paulsem@users.noreply.github.com>
2020-03-24 14:34:05 +0200

    new_version

--- Lab6-WCF_EF/GUI/Form1.Designer.cs
+++ Lab6-WCF_EF/GUI/Form1.Designer.cs
@@ -28,21 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dgp = new System.Windows.Forms.DataGridView();
+            this.dgc = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgp)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgc)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dgp
+            // 
+            this.dgp.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgp.Location = new System.Drawing.Point(12, 12);
+            this.dgp.Name = "dgp";
+            this.dgp.RowHeadersWidth = 51;
+            this.dgp.RowTemplate.Height = 24;
+            this.dgp.Size = new System.Drawing.Size(329, 293);
+            this.dgp.TabIndex = 0;
+            // 
+            // dgc
+            // 
+            this.dgc.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgc.Location = new System.Drawing.Point(362, 12);
+            this.dgc.Name = "dgc";
+            this.dgc.RowHeadersWidth = 51;
+            this.dgc.RowTemplate.Height = 24;
+            this.dgc.Size = new System.Drawing.Size(345, 293);
+            this.dgc.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgc);
+            this.Controls.Add(this.dgp);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgp)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgc)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgp;
+        private System.Windows.Forms.DataGridView dgc;
     }
 }
 
--- Lab6-WCF_EF/GUI/Form1.cs
+++ Lab6-WCF_EF/GUI/Form1.cs
@@ -13,6 +13,7 @@ namespace GUI
 {
     public partial class Form1 : Form
     {
+        List<Post> posts = new List<Post>();
         public Form1()
         {
             InitializeComponent();
@@ -20,16 +21,17 @@ namespace GUI
 
         private void Form1_Load(object sender, EventArgs e)
         {
+  
             posts = LoadPosts().ToList<Post>();
             dgp.DataSource = posts;
             dgp.Columns[0].Width = 0;
             if (dgp.Rows.Count > 0)
                 dgc.DataSource = posts[0].Comments;
         }
-        private static PostComment.Post[] LoadPosts()
+        private static Lab6_WCF_EF.Post[] LoadPosts()
         {
             PostCommentClient pc = new PostCommentClient();
-            PostComment.Post[] p = pc.GetPosts();
+            Lab6_WCF_EF.Post[] p = pc.GetPosts();
             return p;
         }
     }
